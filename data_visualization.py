0f35b9c7fad7eb80e9c1338b69a4e8e19cf0aa90
Anapayaan <157632675+Anapayaan@users.noreply.github.com>
2024-03-26 15:13:43 -0400

    main function

--- data_visualization.py
+++ data_visualization.py
@@ -1,1 +1,6 @@
-# This file will contain basic funtions to visualize the data in the HDF5 files.
+# This file will contain basic functions to visualize the data in the HDF5 files.
+
+
+if __name__ == "__main__":
+
+    print('hello')
