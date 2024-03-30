8c1b3bbe7598637f4b6953d7df85fca811cdf93d
Hendrix Gryspeerdt <95045551+hendrixgg@users.noreply.github.com>
2024-03-29 20:58:44 -0400

    saved basic person group data to hdf5

--- data_collection.py
+++ data_collection.py
@@ -27,18 +27,42 @@ def create_hdf5():
     """
     Create one HDF5 file for all the data.
     """
-    with h5py.File('sensor_data.hdf5', 'w') as f:
+    with h5py.File('./sensor_data.hdf5', 'a') as f:
         # Create a group for each person.
         for person in os.listdir('./sensor_data/csv'):
             person_group = f.create_group(person)
             # Create a dataset for each action (walking, jumping) and phone position. Then, store the data from csv's in it.
-            for action in os.listdir('./sensor_data/csv/' + person):
-                for dir in os.listdir('./sensor_data/csv/' + person + '/' + action):
+            for action in os.listdir(f'./sensor_data/csv/{person}'):
+                for dir in os.listdir(f'./sensor_data/csv/{person}/{action}'):
                     phone_position = dir.split(' ')[0]
                     df = pd.read_csv(
-                        './sensor_data/csv/' + person + '/' + action + '/' + dir)
+                        f'./sensor_data/csv/{person}/{action}/{dir}/Raw Data.csv')
+                    # Store sensor data in person group.
                     person_group.create_dataset(
                         f'{action}_{phone_position}', data=df)
+                    # Now convert to 5 second intervals.
+
+        # Create a group for the useably organized dataset.
+        # dataset_group = f.create_group('dataset')
+        # Train_group = dataset_group.create_group('Train')
+        # Test_group = dataset_group.create_group('Test')
+
+
+"""
+- take each table of data for a particular (action, phone_position)
+    
+    - create an empty list of all the 5 second windows
+    - now want to iterate and pick out all 5s windows
+        - initialize 2 pointers i, j at first row
+        - while time[j] - time[i] < 5: j++
+        - now the interval size is 5 seconds, interval = [[ti, xi, yi, zi], ..., [ti, xj, yj, zj]]
+        - save interval.flatten()
+    
+
+
+
+
+"""
 
 
 if __name__ == "__main__":
